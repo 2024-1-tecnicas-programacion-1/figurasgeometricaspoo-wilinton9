62ffc7491855729c6627083d7b873369d9898dd5
AlejoPzk <alejopalis0@gmail.com>
2024-05-09 09:11:38 -0500

    se adiciono el codigo para cada class

--- src/main/java/com/mycompany/figurasgeometricaspoo/Circulo.java
+++ src/main/java/com/mycompany/figurasgeometricaspoo/Circulo.java
@@ -1,5 +1,20 @@
 package com.mycompany.figurasgeometricaspoo;
 
-public class Circulo {
-    
+public class Circulo extends FiguraGeometrica {
+    private final double radio;
+
+    public Circulo(String nombre, String color, double radio) {
+        super(nombre, color);
+        this.radio = radio;
+    }
+
+    @Override
+    public double obtenerArea() {
+        return Math.PI * radio * radio;
+    }
+
+    @Override
+    public double obtenerPerimetro() {
+        return 2 * Math.PI * radio;
+    }
 }
